0f3c72a02192bbf7ee4e4ade85f7b4362b1dcf0f
Karina <karinadgarces@gmail.com>
2022-02-09 22:38:21 -0500

    winner is now declared

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -56,12 +56,15 @@ class Board extends React.Component {
   }
   handleClick(i){
     const squares = this.state.squares.slice();
+    if(calculateWinner(squares) || squares[i]){
+        return
+      }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares:squares,
       xIsNext: !this.state.xIsNext})
       //this will allow x & o to take turns
-   
+  
   }
   renderSquare(i) {
     return <Square value={this.state.squares[i]}
@@ -70,7 +73,15 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if(winner){
+      status = "Winner:" + winner;
+    } else {
+     status = 'Next player: ' + (this.state.xIsNext ? "X" : "O");
+    }
+    
+    //ternary expressions
 
     return (
       <div>
@@ -111,6 +122,8 @@ class Game extends React.Component {
   }
 }
 
+
+
 // ========================================
 
 ReactDOM.render(
@@ -118,7 +131,30 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
